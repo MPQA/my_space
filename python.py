ed863ad21db173afd4e6314d14fb87218153a449
mpqa <mpqa111@gmail.com>
2021-02-20 00:41:54 +0200

    first commit

---
+++ python.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# int целые числа как + так и -
+a = 30;
+dog = 10;
+book = 45;
+line = '_________________'
+print (a, dog, book)
+print (line)
+
+# float дробные числа
+volume = 12.5
+vodka = 0.75
+salary = 1488.01
+p = float(22)
+print (volume, vodka, salary, p)
+
+print (line)
+
+#string # str
+name = "Misha"
+fio = "Misha Past Wladinir"
+print (name, fio)
+print (line)
+
+#bool
+status = True
+status = False
+print (status)
+print (line)
+
+#comlex
+a1 = 44
+f_1 = 100
+print (a1, f_1)
+print (line)
+
+x = 'Hello World'
+print (x)
+print (line)
+
+print (type (a))
+print (type (vodka))
+print (type (fio))
+print (type (status))
+print (type (a1))
+print (type (x))
+
+x=y=z=0
+print (x)
+print (y)
+print (z)
+print (line)
