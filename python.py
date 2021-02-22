bc9a4762dfcef2f4e149dbc2f9bc5d28d8ba61cd
mpqa <mpqa111@gmail.com>
2021-02-22 23:35:10 +0200

    2commit. цыклы

--- python.py
+++ python.py
@@ -53,5 +53,60 @@ print (y)
 print (z)
 print (line)
 
+q1 = 10
+q2 = 2+4
+q3 = 44
+q4 = 2.5
+
+w = q1 * q4
+w1 = q1 / q4
+print ('w\n',w1)
+print (line)
+
+#/n новая строка
+text = 'The GLOCK G18C -\ncomes with compensator slots in the barrel and slide.'
+print (text)
+print (line)
+
+print(f"Hello\nWorld!")
+print (line)
+print(f"Hello\rWorld!")
+print (line)
+
+#множественное присваивание
+x1,x2,x3,x4 = 1,2,5,8
+c1=x2*x3
+c2=x4/x2
+print (x1,x2,x3,x4,c1,c2)
+c3 = int(c2)
 
 print (line)
+
+tmp1 = x1
+tmp2 = x2
+x1 = tmp2
+x2 = tmp1
+
+print (x1,x2,x3,x4)
+print (line)
+
+r1 = x1+x3
+r2 = x2+x4
+
+print (r1,r2) #7і9
+print (line)
+
+#x1,x2 = x3,x4
+print (x1,x2,x3,x4)
+print (line)
+
+x1,x2 = x2,x1
+print (x1,x2)
+print (line)
+
+# в квадрате. поднести в степень
+# ДВОЙНОЕ УМНОЖЕНИЕ ЭТО ВОЗВЕДЕНИЕ В СТЕПЕНЬ
+e1 = 2**2 #в степень 2
+e2 = 12**2
+e3 = 9**0.5 # так ПОДНЕСТИ К КОРНЮ
+print (e1, e2, e3)
