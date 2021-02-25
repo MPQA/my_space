200927b4be1d42777edd24b351877a07208a6749
mpqa <mpqa111@gmail.com>
2021-02-25 17:56:07 +0200

    commit 2. nwe file w3.py

---
+++ w3.py
@@ -0,0 +1,39 @@
+line = "_______________"
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+print (line)
+
+a = "Hello, World!"
+print(a[0])
+
+print (line)
+
+for x in "banana":
+    print(x)
+
+print (line)
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+print (line)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+print (line)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+# p = "Hello, World!"
+#  print(len(p))
